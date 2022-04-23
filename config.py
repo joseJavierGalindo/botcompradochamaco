81ea41da3c3620c138cf7c54ac79ee743bbff6c3
joseJavierGalindo <100740011+joseJavierGalindo@users.noreply.github.com>
2022-04-23 17:42:14 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5395226273:AAFIPqSuFimvVTzAmSa1foe1rh-DQaKEKt4'
+API_ID = 16622951
+API_HASH = '21b795f8112a5546e9e32e7358e9d401'
+
+
+# DATOS DEL MOODLE
+USUARIO = 'darian.borges@estudiantes.fbio.uh.cu'
+PASSWORD = 'darian1995'
+USUARIO_ID = 32581
+ZIP_MB = 245
+MOODLE_URL = 'moodle.uclv.edu.cu'
+OWNER = [1007430305]
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'yanco148@gmail.com'
+PASS_MEGA = 'yancarlos1482005'
